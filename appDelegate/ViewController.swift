c78fccf2fc028b27f962758401312f07b4ef7758
Steven Wang <steven@stevens-macbook-pro.local>
2016-05-10 16:41:18 +0800

    1st

--- appDelegate/ViewController.swift
+++ appDelegate/ViewController.swift
@@ -8,18 +8,42 @@
 
 import UIKit
 
+var appcount : Int = 0
+
+
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var countLabel: UILabel!
+    
+    func increase() {
+        appcount = appcount + 2
+        countLabel.text = String(appcount)
+        
+    }
+    
+    func decrease(){
+        appcount = appcount - 1
+        countLabel.text = String(appcount)
+        
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector (ViewController.increase), name: UIApplicationDidBecomeActiveNotification , object: nil)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.decrease), name: UIApplicationDidEnterBackgroundNotification, object: nil)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
